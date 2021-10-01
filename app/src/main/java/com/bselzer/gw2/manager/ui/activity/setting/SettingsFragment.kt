3c6a2ea6d11d4328487e9d344911b5c2863dc55f
Woody230 <bselzer1@outlook.com>
2021-09-30 21:46:14 -0400

    Token preference

--- app/src/main/java/com/bselzer/gw2/manager/ui/activity/setting/SettingsFragment.kt
+++ app/src/main/java/com/bselzer/gw2/manager/ui/activity/setting/SettingsFragment.kt
@@ -1,36 +1,65 @@
 package com.bselzer.gw2.manager.ui.activity.setting
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.preference.EditTextPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.PreferenceScreen
 import com.bselzer.gw2.manager.R
 import com.bselzer.gw2.manager.companion.AppCompanion
+import com.bselzer.gw2.manager.companion.PreferenceCompanion
+import com.bselzer.library.kotlin.extension.coroutine.showToast
 import com.bselzer.library.kotlin.extension.preference.DataStoreWrapper
+import com.bselzer.library.kotlin.extension.preference.addTo
+import com.bselzer.library.kotlin.extension.preference.clear
+import com.bselzer.library.kotlin.extension.preference.update
+import io.ktor.client.features.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         val context = preferenceManager.context
-        preferenceManager.preferenceDataStore = DataStoreWrapper(AppCompanion.DATASTORE)
+        val datastore = AppCompanion.DATASTORE
+        preferenceManager.preferenceDataStore = DataStoreWrapper(datastore)
 
         val screen = preferenceManager.createPreferenceScreen(context)
 
-        // TODO store token instead of api key
         EditTextPreference(context).apply {
-            key = "ApiKey"
+            key = PreferenceCompanion.TOKEN.name
             summaryProvider = EditTextPreference.SimpleSummaryProvider.getInstance()
-            title = "API Key"
+            title = "Token"
             setIcon(R.drawable.gw2_black_lion_key)
-            dialogTitle = "API Key"
+            dialogTitle = "Token"
             setDialogIcon(R.drawable.gw2_black_lion_key)
+            onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
+                CoroutineScope(Dispatchers.Main).launch {
+                    // Validate the new token before committing it.
+                    val token = newValue?.toString()?.trim() ?: ""
+                    if (token.isBlank()) {
+                        datastore.clear(PreferenceCompanion.TOKEN, this)
+                        text = token
+                        return@launch
+                    }
+
+                    try {
+                        withContext(Dispatchers.IO) {
+                            AppCompanion.GW2.token.information(token = token)
+                        }
+
+                        datastore.update(PreferenceCompanion.TOKEN, token, this)
+                        text = token
+                    } catch (ex: ClientRequestException) {
+                        showToast(context, "Unable to save token: ${ex.response.readText()}", Toast.LENGTH_LONG)
+                    }
+                }
+                return@OnPreferenceChangeListener false
+            }
         }.addTo(screen)
 
         preferenceScreen = screen
     }
-
-    /**
-     * Adds [this] to the [screen]
-     */
-    private fun Preference.addTo(screen: PreferenceScreen) = screen.addPreference(this)
 }
--- app/src/main/java/com/bselzer/library/kotlin/extension/coroutine/LaunchExtension.kt
+++ app/src/main/java/com/bselzer/library/kotlin/extension/coroutine/LaunchExtension.kt
@@ -1,16 +1,25 @@
 package com.bselzer.library.kotlin.extension.coroutine
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import android.content.Context
+import android.widget.Toast
+import kotlinx.coroutines.*
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 
+/**
+ * Repeats the [block] every [interval].
+ */
 @OptIn(ExperimentalTime::class)
 fun CoroutineScope.repeat(interval: Duration, block: suspend CoroutineScope.() -> Unit): Job = launch {
     while (true) {
         block(this)
         delay(interval)
     }
+}
+
+/**
+ * Shows a Toast using the main coroutine context.
+ */
+suspend fun CoroutineScope.showToast(context: Context, text: CharSequence, duration: Int) = withContext(Dispatchers.Main) {
+    Toast.makeText(context, text, duration).show()
 }

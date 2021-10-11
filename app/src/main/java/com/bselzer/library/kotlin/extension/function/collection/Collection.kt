6f794d7a215e7adba5ceee0092c74b626d2ff7f8
Woody230 <bselzer1@outlook.com>
2021-10-10 23:02:38 -0400

    Move stuff to KotlinExtensions

--- app/src/main/java/com/bselzer/library/kotlin/extension/function/collection/Collection.kt
+++
@@ -1,6 +0,0 @@
-package com.bselzer.library.kotlin.extension.function.collection
-
-/**
- * Adds [this] to the collection.
- */
-fun <T> T.addTo(collection: MutableCollection<T>) = collection.add(this)

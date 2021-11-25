581c341bfabb63cee98c9b1208aa81bad320b2e8
Woody230 <bselzer1@outlook.com>
2021-11-25 07:30:05 -0500

    Selected page -> survive activity recreation

--- app/src/main/java/com/bselzer/gw2/manager/ui/activity/wvw/WvwActivity.kt
+++ app/src/main/java/com/bselzer/gw2/manager/ui/activity/wvw/WvwActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -284,7 +285,7 @@ class WvwActivity : BaseActivity() {
 
     @Composable
     private fun Content() = AppTheme {
-        when (remember { selectedPage }.value) {
+        when (rememberSaveable { selectedPage }.value) {
             MAP -> ShowMapPage()
             MATCH -> ShowMatchPage()
             DETAILED_SELECTED_OBJECTIVE -> ShowSelectedObjectivePage()

7b26a84d76906c57d1ba70bc92129e66b3ead178
Woody230 <bselzer1@outlook.com>
2022-05-16 10:41:19 -0400

    1. missing internet permissions 2. locked transactions

--- common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/repository/WorldRepository.kt
+++ common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/repository/WorldRepository.kt
@@ -2,23 +2,20 @@ package com.bselzer.gw2.manager.common.repository
 
 import com.bselzer.gw2.manager.common.dependency.RepositoryDependencies
 import com.bselzer.gw2.v2.model.world.World
-import com.bselzer.ktx.kodein.db.transaction.transaction
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.zip
 
-class WorldRepository(dependencies: RepositoryDependencies) : RepositoryDependencies by dependencies {
+class WorldRepository(dependencies: RepositoryDependencies) : AppRepository(dependencies) {
     fun worlds(): Flow<Collection<World>> = flow {
-        caches.database.transaction().use {
+        lockedTransaction {
             with(caches.gw2.world) {
                 emit(findWorlds())
             }
         }
     }
 
-    fun selectedWorld(): Flow<World?> = worlds().map { worlds ->
-        worlds.firstOrNull { world ->
-            world.id == preferences.wvw.selectedWorld.get()
-        }
+    fun selectedWorld(): Flow<World?> = preferences.wvw.selectedWorld.observeOrNull().zip(worlds()) { selectedId, worlds ->
+        worlds.firstOrNull { world -> world.id == selectedId }
     }
 }
--- common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/repository/WvwRepository.kt
+++ common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/repository/WvwRepository.kt
@@ -2,16 +2,16 @@ package com.bselzer.gw2.manager.common.repository
 
 import com.bselzer.gw2.manager.common.dependency.RepositoryDependencies
 import com.bselzer.gw2.v2.model.wvw.match.WvwMatch
-import com.bselzer.ktx.kodein.db.transaction.transaction
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-class WvwRepository(dependencies: RepositoryDependencies) : RepositoryDependencies by dependencies {
+class WvwRepository(dependencies: RepositoryDependencies) : AppRepository(dependencies) {
     fun selectedMatch(): Flow<WvwMatch?> = flow {
-        caches.database.transaction().use {
+        lockedTransaction {
             with(caches.gw2.wvw) {
                 val worldId = preferences.wvw.selectedWorld.get()
-                emit(findMatch(worldId))
+                val match = if (worldId.isDefault) null else findMatch(worldId)
+                emit(match)
             }
         }
     }

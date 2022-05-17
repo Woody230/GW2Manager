06b002994f5bf69b65127d452560ceb985b46b75
Woody230 <bselzer1@outlook.com>
2022-05-17 14:08:18 -0400

    Use updated drawer projector

--- common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/host/content/DrawerComposition.kt
+++ common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/host/content/DrawerComposition.kt
@@ -1,20 +1,16 @@
 package com.bselzer.gw2.manager.common.ui.layout.host.content
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.dp
 import com.bselzer.gw2.manager.common.ui.base.ViewModelComposition
 import com.bselzer.gw2.manager.common.ui.layout.host.model.drawer.DrawerComponent
 import com.bselzer.gw2.manager.common.ui.layout.host.viewmodel.DrawerViewModel
 import com.bselzer.ktx.compose.resource.images.painter
+import com.bselzer.ktx.compose.ui.layout.drawer.component.DrawerComponentInteractor
+import com.bselzer.ktx.compose.ui.layout.drawer.modal.ModalDrawerInteractor
+import com.bselzer.ktx.compose.ui.layout.drawer.modal.ModalDrawerProjector
+import com.bselzer.ktx.compose.ui.layout.drawer.section.DrawerSectionInteractor
 import com.bselzer.ktx.compose.ui.layout.icon.IconInteractor
-import com.bselzer.ktx.compose.ui.layout.icon.IconPresenter
-import com.bselzer.ktx.compose.ui.layout.icontext.IconTextInteractor
-import com.bselzer.ktx.compose.ui.layout.icontext.IconTextPresenter
-import com.bselzer.ktx.compose.ui.layout.modaldrawer.ModalDrawerInteractor
-import com.bselzer.ktx.compose.ui.layout.modaldrawer.ModalDrawerPresenter
-import com.bselzer.ktx.compose.ui.layout.modaldrawer.ModalDrawerProjector
 import com.bselzer.ktx.compose.ui.layout.modifier.interactable.Clickable
-import com.bselzer.ktx.compose.ui.layout.modifier.presentable.ModularSize
 import com.bselzer.ktx.compose.ui.layout.text.TextInteractor
 import dev.icerock.moko.resources.compose.localized
 
@@ -23,20 +19,31 @@ class DrawerComposition : ViewModelComposition<DrawerViewModel>() {
     override fun Content(model: DrawerViewModel) = model.run {
         ModalDrawerProjector(
             interactor = ModalDrawerInteractor(
-                components = icons.map { icons -> icons.map { icon -> icon.iconTextInteractor() } }
+                sections = listOf(wvwSection(), settingsSection(), aboutSection())
             ),
-            presenter = ModalDrawerPresenter(
-                component = IconTextPresenter(
-                    icon = IconPresenter(modifier = ModularSize(24.dp, 24.dp))
-                )
-            )
-        ).drawerContent()
+        ).DrawerContent()
     }
 
     @Composable
-    private fun DrawerComponent.iconTextInteractor(): IconTextInteractor {
+    private fun DrawerViewModel.wvwSection() = DrawerSectionInteractor(
+        title = TextInteractor(text = wvwTitle.localized()),
+        components = listOf(wvwMap.interactor(), wvwMatch.interactor())
+    )
+
+    @Composable
+    private fun DrawerViewModel.settingsSection() = DrawerSectionInteractor(
+        components = listOf(settings.interactor(), cache.interactor())
+    )
+
+    @Composable
+    private fun DrawerViewModel.aboutSection() = DrawerSectionInteractor(
+        components = listOf(license.interactor(), about.interactor())
+    )
+
+    @Composable
+    private fun DrawerComponent.interactor(): DrawerComponentInteractor {
         val localized: String = description.localized()
-        return IconTextInteractor(
+        return DrawerComponentInteractor(
             icon = IconInteractor(
                 painter = icon.painter(),
                 contentDescription = localized
--- common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/host/viewmodel/DrawerViewModel.kt
+++ common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/host/viewmodel/DrawerViewModel.kt
@@ -6,48 +6,45 @@ import com.bselzer.gw2.manager.common.ui.base.ViewModel
 import com.bselzer.gw2.manager.common.ui.layout.host.model.drawer.DrawerComponent
 import com.bselzer.gw2.manager.common.ui.layout.main.configuration.MainConfig
 import com.bselzer.ktx.resource.Resources
+import dev.icerock.moko.resources.desc.StringDesc
 import dev.icerock.moko.resources.desc.desc
 
 class DrawerViewModel(context: AppComponentContext) : ViewModel(context) {
-    private val wvwMap = DrawerComponent(
+    val wvwTitle: StringDesc = Gw2Resources.strings.wvw.desc()
+
+    val wvwMap = DrawerComponent(
         icon = Gw2Resources.images.gw2_rank_dolyak,
         description = Gw2Resources.strings.wvw_map.desc(),
         configuration = MainConfig.WvwMapConfig
     )
 
-    private val wvwMatch = DrawerComponent(
+    val wvwMatch = DrawerComponent(
         icon = Gw2Resources.images.gw2_rank_dolyak,
         description = Gw2Resources.strings.wvw_match.desc(),
         configuration = MainConfig.WvwMatchConfig
     )
 
-    private val settings = DrawerComponent(
+    val settings = DrawerComponent(
         icon = Gw2Resources.images.ic_settings,
         description = Resources.strings.settings.desc(),
         configuration = MainConfig.SettingsConfig
     )
 
-    private val cache = DrawerComponent(
+    val cache = DrawerComponent(
         icon = Gw2Resources.images.ic_cached,
         description = Resources.strings.cache.desc(),
         configuration = MainConfig.CacheConfig
     )
 
-    private val license = DrawerComponent(
+    val license = DrawerComponent(
         icon = Gw2Resources.images.ic_policy,
         description = Resources.strings.licenses.desc(),
         configuration = MainConfig.LicenseConfig
     )
 
-    private val about = DrawerComponent(
+    val about = DrawerComponent(
         icon = Gw2Resources.images.ic_info,
         description = Resources.strings.about_app.desc(),
         configuration = MainConfig.AboutConfig
-    )
-
-    val icons: List<List<DrawerComponent>> = listOf(
-        listOf(wvwMap, wvwMatch),
-        listOf(settings, cache),
-        listOf(license, about)
     )
 }

5f28702cc1a8e6272452d56d128a0895b4eb6646
Woody230 <bselzer1@outlook.com>
2022-05-30 12:22:44 -0400

    image get locking

--- common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/repository/instance/generic/ImageRepository.kt
+++ common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/repository/instance/generic/ImageRepository.kt
@@ -3,18 +3,22 @@ package com.bselzer.gw2.manager.common.repository.instance.generic
 import com.bselzer.gw2.manager.common.dependency.RepositoryDependencies
 import com.bselzer.gw2.manager.common.repository.instance.AppRepository
 import com.bselzer.ktx.compose.image.model.Image
+import com.bselzer.ktx.coroutine.sync.LockByKey
 import com.bselzer.ktx.kodein.db.operation.getById
 import com.bselzer.ktx.kodein.db.transaction.transaction
 
 class ImageRepository(
     dependencies: RepositoryDependencies
 ) : AppRepository(dependencies) {
+    private val lock = LockByKey<String>()
+
     suspend fun getImage(url: String) = database.transaction().use {
         getById(
             id = url,
             requestSingle = {
-                // TODO lock based on url
-                clients.image.getImageOrNull(url) ?: Image(url, byteArrayOf())
+                lock.withLock(url) {
+                    clients.image.getImageOrNull(url) ?: Image(url, byteArrayOf())
+                }
             },
             writeFilter = { image -> image.content.isNotEmpty() }
         )

0ce8a59fb44ff98725fe31ea81726b6edd15755f
Woody230 <bselzer1@outlook.com>
2022-06-06 19:26:15 -0400

    jvm try/finally to dispose of database

--- desktop/src/jvmMain/kotlin/com/bselzer/gw2/manager/desktop/Main.kt
+++ desktop/src/jvmMain/kotlin/com/bselzer/gw2/manager/desktop/Main.kt
@@ -14,29 +14,33 @@ fun main() {
     // Initialize dependencies before composing since they won't change.
     val app = JvmApp()
 
-    // Initialize the component context before composing to avoid potentially creating on another thread.
-    // https://arkivanov.github.io/Decompose/component/overview/#root-componentcontext-in-jetpackjetbrains-compose
-    val context = Gw2ComponentContext(
-        dependencies = app.dependencies,
-        component = DefaultComponentContext(LifecycleRegistry())
-    )
+    try {
+        // Initialize the component context before composing to avoid potentially creating on another thread.
+        // https://arkivanov.github.io/Decompose/component/overview/#root-componentcontext-in-jetpackjetbrains-compose
+        val context = Gw2ComponentContext(
+            dependencies = app.dependencies,
+            component = DefaultComponentContext(LifecycleRegistry())
+        )
 
-    val host = HostViewModel(context)
-    application {
-        Window(
-            title = AppResources.strings.app_name.localized(),
-            onCloseRequest = ::exitApplication
-        ) {
-            // TODO tailor to desktop
-            //  components can be split into multiple columns due to bigger screen size
-            //  scaffold modal drawer should always be visible by default and only wrap width
-            //  constrained dialog instead of normal alert dialog will create a window with a title (of "Untitled")
-            //  dialog size and scrolling
-            //  snackbar not showing
-            //  back handling via escape
-            app.Content {
-                HostComposition(host).Content()
+        val host = HostViewModel(context)
+        application {
+            Window(
+                title = AppResources.strings.app_name.localized(),
+                onCloseRequest = ::exitApplication
+            ) {
+                // TODO tailor to desktop
+                //  components can be split into multiple columns due to bigger screen size
+                //  scaffold modal drawer should always be visible by default and only wrap width
+                //  constrained dialog instead of normal alert dialog will create a window with a title (of "Untitled")
+                //  dialog size and scrolling
+                //  snackbar not showing
+                //  back handling via escape
+                app.Content {
+                    HostComposition(host).Content()
+                }
             }
         }
+    } finally {
+        app.dependencies.database.close()
     }
 }

3f4a15f1af4e6df67a4e63c8f4369a51d8c9ff52
Woody230 <bselzer1@outlook.com>
2022-06-08 06:48:51 -0400

    Move library sort/distinct to ktx

--- common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/main/viewmodel/LicenseViewModel.kt
+++ common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/main/viewmodel/LicenseViewModel.kt
@@ -1,7 +1,8 @@
 package com.bselzer.gw2.manager.common.ui.layout.main.viewmodel
 
 import com.bselzer.gw2.manager.common.ui.base.AppComponentContext
-import com.bselzer.ktx.library.author
+import com.bselzer.ktx.library.distinct
+import com.bselzer.ktx.library.sorted
 import com.bselzer.ktx.resource.KtxResources
 import com.mikepenz.aboutlibraries.entity.Library
 import dev.icerock.moko.resources.desc.StringDesc
@@ -9,7 +10,5 @@ import dev.icerock.moko.resources.desc.desc
 
 class LicenseViewModel(context: AppComponentContext) : MainViewModel(context) {
     override val title: StringDesc = KtxResources.strings.licenses.desc()
-    override val libraries: List<Library> = super.libraries
-        .distinctBy { library -> library.name.trim() + library.author.trim() + (library.artifactVersion ?: "").trim() }
-        .sortedWith(compareBy(String.CASE_INSENSITIVE_ORDER) { library -> library.name })
+    override val libraries: List<Library> = super.libraries.sorted().distinct()
 }

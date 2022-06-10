3dfcaedd9bb99d902d91ec1a4e071f98515383cf
Woody230 <bselzer1@outlook.com>
2022-06-09 20:58:45 -0400

    IntIdentifierSetting

--- common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/preference/WvwPreference.kt
+++ common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/preference/WvwPreference.kt
@@ -1,9 +1,9 @@
 package com.bselzer.gw2.manager.common.preference
 
 import com.bselzer.gw2.v2.model.world.WorldId
+import com.bselzer.ktx.settings.setting.IntIdentifierSetting
 import com.bselzer.ktx.settings.setting.SerializableSetting
 import com.bselzer.ktx.settings.setting.Setting
-import com.bselzer.ktx.settings.setting.SettingWrapper
 import com.russhwolf.settings.ExperimentalSettingsApi
 import com.russhwolf.settings.coroutines.SuspendSettings
 import kotlinx.datetime.Instant
@@ -22,13 +22,8 @@ class WvwPreference(settings: SuspendSettings) {
      */
     val lastRefresh: SerializableSetting<Instant> = SerializableSetting(settings = settings, key = "WvwLastRefresh", defaultValue = Instant.DISTANT_FUTURE, serializer())
 
-    // TODO value class settings
     /**
      * The id of the selected world.
      */
-    val selectedWorld: Setting<WorldId> = object : SettingWrapper<WorldId>(settings = settings, key = "SelectedWorld", defaultValue = WorldId()) {
-        override suspend fun get(): WorldId = WorldId(settings.getInt(key, defaultValue.value))
-        override suspend fun getOrNull(): WorldId? = settings.getIntOrNull(key)?.let { WorldId(it) }
-        override suspend fun put(value: WorldId) = settings.putInt(key, value.value)
-    }
+    val selectedWorld: Setting<WorldId> = IntIdentifierSetting(settings = settings, key = "SelectedWorld", create = { WorldId(it) })
 }

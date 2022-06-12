dd43f24913736e68fa2c068cf4742f902df5eeab
Woody230 <bselzer1@outlook.com>
2022-06-12 06:10:27 -0400

    Remove pinch to zoom

--- common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/main/content/map/ViewerComposition.kt
+++ common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/main/content/map/ViewerComposition.kt
@@ -1,13 +1,10 @@
 package com.bselzer.gw2.manager.common.ui.layout.main.content.map
 
-import androidx.compose.foundation.gestures.rememberTransformableState
-import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -18,22 +15,14 @@ import com.bselzer.gw2.manager.common.ui.layout.main.content.map.viewer.Platform
 import com.bselzer.gw2.manager.common.ui.layout.main.viewmodel.map.ViewerViewModel
 import com.bselzer.ktx.compose.resource.strings.localized
 import com.bselzer.ktx.compose.ui.layout.background.image.BackgroundImage
-import kotlinx.coroutines.launch
 
 class ViewerComposition(model: ViewerViewModel) : ViewModelComposition<ViewerViewModel>(model) {
     @Composable
     override fun ViewerViewModel.Content() {
         // NOTE: Intentionally not using a background in case the device size is large enough that the map doesn't match the full size.
 
-        val scope = rememberCoroutineScope()
-        val pinchToZoom = rememberTransformableState { zoomChange, panChange, rotationChange ->
-            // Allow the user to change the zoom by pinching the map.
-            val change = if (zoomChange > 1) 1 else -1
-            scope.launch { changeZoom(change) }
-        }
-
         ConstraintLayout(
-            modifier = Modifier.fillMaxSize().transformable(pinchToZoom)
+            modifier = Modifier.fillMaxSize()
         ) {
             val (map, selectedObjective) = createRefs()
             PlatformGridComposition(model).Content(

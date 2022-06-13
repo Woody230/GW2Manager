881278dde4496a413d25c1fd6aa04dff463a2d9c
Woody230 <bselzer1@outlook.com>
2022-06-12 22:32:17 -0400

    Enable refresh grid => BoxGrid

--- common/src/androidMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/main/content/map/viewer/GridComposition.kt
+++ common/src/androidMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/main/content/map/viewer/GridComposition.kt
@@ -2,4 +2,8 @@ package com.bselzer.gw2.manager.common.ui.layout.main.content.map.viewer
 
 import com.bselzer.gw2.manager.common.ui.layout.main.viewmodel.map.ViewerViewModel
 
-actual fun PlatformGridComposition(model: ViewerViewModel): GridComposition = MapComposeGridComposition(model)
+actual fun PlatformGridComposition(model: ViewerViewModel): GridComposition = if (model.configuration.wvw.map.refreshGrid) {
+    BoxGridComposition(model)
+} else {
+    MapComposeGridComposition(model)
+}

790eb30ce9b68905e0305ba052f393116d3efe5d
Woody230 <bselzer1@outlook.com>
2022-06-26 06:33:19 -0400

    Contested areas neutral links

--- common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/contestedarea/content/ContestedAreasComposition.kt
+++ common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/contestedarea/content/ContestedAreasComposition.kt
@@ -2,6 +2,7 @@ package com.bselzer.gw2.manager.common.ui.layout.contestedarea.content
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -9,7 +10,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import com.bselzer.gw2.manager.common.ui.base.ViewModelComposition
+import com.bselzer.gw2.manager.common.ui.layout.common.BorderedCard
 import com.bselzer.gw2.manager.common.ui.layout.contestedarea.model.ContestedObjective
+import com.bselzer.gw2.manager.common.ui.layout.contestedarea.model.ContestedObjectives
 import com.bselzer.gw2.manager.common.ui.layout.contestedarea.viewmodel.ContestedAreasViewModel
 import com.bselzer.gw2.manager.common.ui.layout.image.AsyncImage
 import com.bselzer.gw2.manager.common.ui.layout.image.Content
@@ -19,14 +22,24 @@ class ContestedAreasComposition(
     model: ContestedAreasViewModel
 ) : ViewModelComposition<ContestedAreasViewModel>(model) {
     @Composable
-    override fun ContestedAreasViewModel.Content() = contestedObjectives.forEach { contestedObjectives ->
-        Row {
-            contestedObjectives.objectives.forEach { objective ->
-                objective.Content(Modifier)
+    override fun ContestedAreasViewModel.Content() = BorderedCard(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column {
+            // TODO constrain
+            contestedObjectives.forEach { contestedObjectives ->
+                contestedObjectives.Content(Modifier)
             }
-
-            // TODO constrain rows and add ppt
         }
+    }
+
+    @Composable
+    private fun ContestedObjectives.Content(modifier: Modifier) = Row(modifier) {
+        objectives.forEach { objective ->
+            objective.Content(Modifier)
+        }
+
+        // TODO ppt
     }
 
     @Composable

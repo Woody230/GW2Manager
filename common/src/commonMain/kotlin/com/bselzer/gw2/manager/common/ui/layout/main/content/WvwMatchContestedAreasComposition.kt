d1d0732abc327db94d9f7a5f4c5812f9b785b5c4
Woody230 <bselzer1@outlook.com>
2022-07-02 08:00:05 -0400

    minor

--- common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/main/content/WvwMatchContestedAreasComposition.kt
+++ common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/main/content/WvwMatchContestedAreasComposition.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.bselzer.gw2.manager.common.ui.layout.borderlands.content.BorderlandsComposition
 import com.bselzer.gw2.manager.common.ui.layout.chart.content.ChartComposition
@@ -40,7 +41,7 @@ class WvwMatchContestedAreasComposition(
 
         // Depending on rotation or window size, adjust whether to display horizontally or vertically.
         if (ApplicationSize.current.width > ApplicationSize.current.height) {
-            Row {
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 chart()
                 contestedAreas()
             }

b259eadb6f00e8596f3943ece609167e48c50cd9
Woody230 <bselzer1@outlook.com>
2023-08-19 08:07:51 -0400

    theme color filter for status image

--- common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/custom/preference/content/StatusComposition.kt
+++ common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/custom/preference/content/StatusComposition.kt
@@ -4,8 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.bselzer.gw2.manager.common.ui.base.ViewModelComposition
 import com.bselzer.gw2.manager.common.ui.layout.custom.preference.viewmodel.StatusViewModel
+import com.bselzer.gw2.manager.common.ui.theme.ThemedColorFilter
+import com.bselzer.ktx.compose.ui.layout.image.ImagePresenter
 import com.bselzer.ktx.compose.ui.layout.preference.PreferenceInteractor
+import com.bselzer.ktx.compose.ui.layout.preference.PreferencePresenter
 import com.bselzer.ktx.compose.ui.layout.preference.switch.SwitchPreferenceInteractor
+import com.bselzer.ktx.compose.ui.layout.preference.switch.SwitchPreferencePresenter
 import com.bselzer.ktx.compose.ui.layout.preference.switch.SwitchPreferenceProjector
 import com.bselzer.ktx.compose.ui.layout.switch.SwitchInteractor
 import com.bselzer.ktx.resource.images.painter
@@ -24,6 +28,9 @@ class StatusComposition(
         interactor = SwitchPreferenceInteractor(
             preference = preferenceInteractor(),
             switch = switchInteractor()
+        ),
+        presenter = SwitchPreferencePresenter(
+            preference = preferencePresenter()
         )
     )
 
@@ -32,6 +39,12 @@ class StatusComposition(
         painter = resources.image.painter(),
         title = resources.title.localized(),
         subtitle = resources.subtitle.localized()
+    )
+
+    @Composable
+    private fun StatusViewModel.preferencePresenter() = PreferencePresenter(
+        // Status image is by default harder to see in dark mode.
+        image = ImagePresenter(colorFilter = ThemedColorFilter)
     )
 
     @Composable

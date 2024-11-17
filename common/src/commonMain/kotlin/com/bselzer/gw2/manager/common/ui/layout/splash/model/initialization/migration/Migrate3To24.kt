2bea637f05e24f38154565d1a1cce65150a4006c
Woody230 <bselzer1@outlook.com>
2024-11-17 09:06:43 -0500

    remove selected world on migration

--- common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/splash/model/initialization/migration/Migrate3To24.kt
+++ common/src/commonMain/kotlin/com/bselzer/gw2/manager/common/ui/layout/splash/model/initialization/migration/Migrate3To24.kt
@@ -6,13 +6,15 @@ import com.bselzer.gw2.v2.model.world.World
 import com.bselzer.ktx.db.operation.clear
 import com.bselzer.ktx.db.transaction.transaction
 
-class Migrate3To23(dependencies: AppDependencies) : Migration(dependencies) {
+class Migrate3To24(dependencies: AppDependencies) : Migration(dependencies) {
     override val from: Int = 3
-    override val to: Int = 23
+    override val to: Int = 24
     override val reason: String = "New worlds created due to world restructuring. The language for translations was being stored with quotes."
 
     override suspend fun migrate(): Unit = database.transaction().use {
         clear<World>()
         clear<Translation>()
+
+        preferences.wvw.selectedWorld.remove()
     }
 }
